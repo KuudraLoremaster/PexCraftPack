ef8f00bf3d440f5aa753051ee4cd3744956f1b0d
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2024-02-11 14:16:55 +0100

    add heavy engineering block

--- groovy/postInit/main.groovy
+++ groovy/postInit/main.groovy
@@ -3,10 +3,19 @@ crafting.addShaped('redstone_engineering_block', item('immersiveengineering:meta
         [metaitem('cableGtSingleTin'), item('gregtech:meta_plate', 2064), metaitem('cableGtSingleTin')],
         [item('gregtech:meta_item_1', 217), item('gregtech:machine', 986), item('gregtech:meta_item_1', 232)], 
         [metaitem('cableGtSingleTin'), item('gregtech:meta_plate', 2064), metaitem('cableGtSingleTin')]
-
-
 ])
 
+crafting.addShaped('heavy_engineering_block', item('immersiveengineering:metal_decoration0', 5) * 1, [
+        [item('gregtech:meta_item_1', 172), item('immersiveengineering:material', 9), item('gregtech:meta_item_1', 172)],
+        [item('gregtech:meta_plate', 324), item('gregtech:machine', 986), item('gregtech:meta_plate', 324)], 
+        [item('gregtech:meta_item_1', 172), item('immersiveengineering:material', 9), item('gregtech:meta_item_1', 172)]
+])
+
+crafting.addShaped('steel_component', item('immersiveengineering:material', 9) * 2, [ //this doesnt work btw
+        [item('gregtech:wire_single', 25),item('gregtech:meta_plate', 324), item('gregtech:wire_single', 25)],
+        [item('gregtech:meta_plate', 324), null, item('gregtech:meta_plate', 324)], 
+        [item('gregtech:wire_single', 25), item('gregtech:meta_plate', 324) item('gregtech:wire_single', 25)]
+])
 // assembler recipes
 recipemap('assembler').recipeBuilder()
         .fluidInputs(liquid('redstone') * 144)
@@ -22,8 +31,8 @@ recipemap('chemical_reactor').recipeBuilder()
         .inputs(item('gregtech:meta_dust', 18),item('gregtech:meta_dust', 103) * 2)
         .outputs(item('pexologicaljourney:pex_ingot') * 12)
         .duration(500)
-        .EUt(64)
+        .EUt(524000)
         .buildAndRegister()
 
 // remove recipes
-crafting.remove(item(/*recipe name here*/)) 
+//crafting.remove(item(recipe name here)) 
