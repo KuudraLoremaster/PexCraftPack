563d98e253898fff34545c5846d184d0b5ef6a73
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2024-02-13 11:37:52 +0100

    id

--- groovy/postInit/recipesToRemove.groovy
+++ groovy/postInit/recipesToRemove.groovy
@@ -2,11 +2,14 @@ def recipesToRemove = [
     'immersiveengineering:metal_decoration/redstone_engineering',
     'immersiveengineering:metal_decoration/light_engineering',
     'immersiveengineering:metal_decoration/heavy_engineering',
+    'immersiveengineering:metal_decoration/generator',
     'immersiveengineering:metal_decoration/radiator',
-    'immersiveengineering:metal_decoration/furnace_heater',
+    'immersiveengineering:metal_devices/furnace_heater',
+    'immersiveengineering:metal_devices/dynamo',
     'immersiveengineering:toolupgrades/drill_waterproof',
     'immersiveengineering:material/component_iron',
-    'immersiveengineering:material/component_steel'
+    'immersiveengineering:material/component_steel',
+    'immersivepetroleum:tool/projector'
 ]
 
 for(name in recipesToRemove) {
