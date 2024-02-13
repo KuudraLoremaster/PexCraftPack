2732d0664d6272b3ddd44225474048945f157b81
KuudraLoremaster <146659154+KuudraLoremaster@users.noreply.github.com>
2024-02-13 19:12:17 +0200

    registered lex and rex

---
+++ groovy/postInit/main.groovy
@@ -0,0 +1,6 @@
+crafting.addShaped('fuckandcock', item('minecraft:diamond') * 1[
+        [metaItem('gregtech:cableGtSingleCopper'), metaItem('gregtech:plateRedstone'), metaItem('gregtech:cableGtSingleCopper')],
+        [metaItem('gregtech:cableGtSingleCopper'), metaItem('gregtech:plateRedstone'), metaItem('gregtech:cableGtSingleCopper')],
+        [metaItem('gregtech:cableGtSingleCopper'), metaItem('gregtech:plateRedstone'), metaItem('gregtech:cableGtSingleCopper')]
+
+])
--- groovy/preInit/main.groovy
+++ groovy/preInit/main.groovy
@@ -1,6 +1,12 @@
 // Ingots
 content.createItem('pex_ingot').register()
+content.createItem('lex_ingot').register()
+content.createItem('rex_ingot').register()
 // Creative Tab
 content.createCreativeTab("pexologicaljourney.creative_tab", item("pexologicaljourney:pex_ingot"))
+content.createCreativeTab("pexologicaljourney.creative_tab", item("pexologicaljourney:lex_ingot"))
+content.createCreativeTab("pexologicaljourney.creative_tab", item("pexologicaljourney:rex_ingot"))
 // Fluids
 content.createFluid('molten_pex').noBlock().setMetalTexture().register()
+content.createFluid('molten_lex').noBlock().setMetalTexture().register()
+content.createFluid('molten_rex').noBlock().setMetalTexture().register()
