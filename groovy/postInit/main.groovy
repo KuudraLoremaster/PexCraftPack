7005f6818546dc85c3fa5c3c824fea71d34a781b
KuudraLoremaster <146659154+KuudraLoremaster@users.noreply.github.com>
2024-02-14 17:32:55 +0200

    fixed pex lex rex recipies but made them use LCR recipies
    now this is bad because the cock cabal one of them is hv PTFE is ev

--- groovy/postInit/main.groovy
+++ groovy/postInit/main.groovy
@@ -1,6 +1,6 @@
-crafting.addShaped('fuckandcock', item('minecraft:diamond') * 1[
-        [metaItem('gregtech:cableGtSingleCopper'), metaItem('gregtech:plateRedstone'), metaItem('gregtech:cableGtSingleCopper')],
-        [metaItem('gregtech:cableGtSingleCopper'), metaItem('gregtech:plateRedstone'), metaItem('gregtech:cableGtSingleCopper')],
-        [metaItem('gregtech:cableGtSingleCopper'), metaItem('gregtech:plateRedstone'), metaItem('gregtech:cableGtSingleCopper')]
+// crafting.addShaped('fuckandcock', item('minecraft:diamond') * 1[
+//         [metaItem('gregtech:cableGtSingleCopper'), metaItem('gregtech:plateRedstone'), metaItem('gregtech:cableGtSingleCopper')],
+//         [metaItem('gregtech:cableGtSingleCopper'), metaItem('gregtech:plateRedstone'), metaItem('gregtech:cableGtSingleCopper')],
+//         [metaItem('gregtech:cableGtSingleCopper'), metaItem('gregtech:plateRedstone'), metaItem('gregtech:cableGtSingleCopper')]
 
-])
+// ])
--- groovy/postInit/recipesMixer.groovy
+++ groovy/postInit/recipesMixer.groovy
@@ -1,9 +1,9 @@
-recipemap('mixer').recipeBuilder()
-        .fluidInputs(fluid('gasoline')*9000)
-        .inputs(item('gregtech:meta_dust', 2) * 3)
-        .fluidOutputs(fluid('napalm')*10000)
-        .duration(80)
-        .EUt(64)
-        .buildAndRegister()
+// recipemap('mixer').recipeBuilder()
+//         .fluidInputs(fluid('gasoline')*9000)
+//         .inputs(item('gregtech:meta_dust', 2) * 3)
+//         .fluidOutputs(fluid('napalm')*10000)
+//         .duration(80)
+//         .EUt(64)
+//         .buildAndRegister()
 
  
