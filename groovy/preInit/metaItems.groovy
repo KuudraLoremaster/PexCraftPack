7c47ac291c18a196206c807b9fcbb57e37cd78f0
KuudraLoremaster <146659154+KuudraLoremaster@users.noreply.github.com>
2024-02-15 23:22:50 +0200

    pex meta item (gone sexual)

--- groovy/preInit/metaItems.groovy
+++ groovy/preInit/metaItems.groovy
@@ -4,16 +4,17 @@ import gregtech.api.items.metaitem.*;
 import gregtech.api.unification.material.MarkerMaterials;
 import gregtech.api.unification.ore.OrePrefix;
 import net.minecraft.util.text.TextFormatting;
+import gregtech.api.unification.material.event.PostMaterialEvent;
 import gregtech.common.items.behaviors.TooltipBehavior;
 
 eventManager.listen {
-    GregTechAPI.PostMaterialEvent event ->
+    PostMaterialEvent event ->
 
         log.infoMC("Adding metaitems...")
 
         StandardMetaItem customMetaItems = new StandardMetaItem((short)2);
         customMetaItems.setRegistryName("meta_item_2")
 
-        customMetaItems.addItem(1, "pex_ingot")
+        customMetaItems.addItem(32001, "pex_ingot").ingot().fluid()
         log.infoMC("Finished adding metaitems")
 }
