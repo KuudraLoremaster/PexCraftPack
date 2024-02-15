45e16665fd5be842ce88f75e5308967f38c83a82
KuudraLoremaster <146659154+KuudraLoremaster@users.noreply.github.com>
2024-02-15 22:39:57 +0200

    pex chain

--- groovy/postInit/chains/pex.groovy
+++ groovy/postInit/chains/pex.groovy
@@ -1,10 +1,41 @@
 // Isopropyl Alcohol
 
 recipemap('chemical_reactor').recipeBuilder()
-        .fluidInputs(liquid('water')*2000,liquid('propene')*1000)
-        .fluidOutputs(fluid('pexologicaljourney:isopropyl_alcohol') * 3000)
+        .fluidInputs(liquid('water') * 2000, liquid('propene')*1000)
+        .fluidOutputs(fluid('isopropyl_alcohol') * 3000)
         .duration(500)
         .EUt(256)
         .buildAndRegister()
 
+
+// Potassium Hydroxide
+recipemap('chemical_reactor').recipeBuilder()
+        .fluidInputs(liquid('water') * 2000)
+        .inputs(item('gregtech:meta_dust', 83) * 2)
+        .fluidOutputs(fluid('hydrogen') * 1000)
+        .outputs(item('pexologicaljourney:potassium_hydroxide'))
+        .duration(200)
+        .EUt(128)
+        .buildAndRegister()
+// Carbon Disulfide
+
+recipemap('large_chemical_reactor').recipeBuilder()
+        .inputs(item('gregtech:meta_dust', 18), item('gregtech:meta_dust', 103) * 2)
+        .notConsumable([metaitem('circuit.integrated').withNbt(["Configuration": 1])])
+        .outputs(item('pexologicaljourney:carbon_disulfide'))
+        .duration(100)
+        .EUt(8)
+        .buildAndRegister()
+
+
+// PEX!!!
+
+recipemap('chemical_reactor').recipeBuilder()
+        .inputs(item('pexologicaljourney:carbon_disulfide') * 1, item('pexologicaljourney:potassium_hydroxide') * 1)
+        .fluidInputs(fluid('isopropyl_alcohol') * 1000)
+        .outputs(item('pexologicaljourney:pex_ingot'))
+        .fluidOutputs(fluid('water') * 1000)
+        .duration(500)
+        .EUt(256)
+        .buildAndRegister()
 
---
+++ groovy/preInit/metaItems.groovy
@@ -0,0 +1,19 @@
+import gregtech.api.GTValues;
+import gregtech.api.GregTechAPI;
+import gregtech.api.items.metaitem.*;
+import gregtech.api.unification.material.MarkerMaterials;
+import gregtech.api.unification.ore.OrePrefix;
+import net.minecraft.util.text.TextFormatting;
+import gregtech.common.items.behaviors.TooltipBehavior;
+
+eventManager.listen {
+    GregTechAPI.PostMaterialEvent event ->
+
+        log.infoMC("Adding metaitems...")
+
+        StandardMetaItem customMetaItems = new StandardMetaItem((short)2);
+        customMetaItems.setRegistryName("meta_item_2")
+
+        customMetaItems.addItem(1, "pex_ingot")
+        log.infoMC("Finished adding metaitems")
+}
