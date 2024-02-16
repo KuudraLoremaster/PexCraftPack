3e0fce71c012ad2353c2daa9249b1d8e13923c85
KuudraLoremaster <146659154+KuudraLoremaster@users.noreply.github.com>
2024-02-17 01:11:08 +0200

    Update pex.groovy changed the recipes to use the meta items

--- groovy/postInit/chains/pex.groovy
+++ groovy/postInit/chains/pex.groovy
@@ -1,36 +1,38 @@
 // Isopropyl Alcohol
+
 recipemap('chemical_reactor').recipeBuilder()
         .fluidInputs(liquid('water') * 2000, liquid('propene')*1000)
-        .fluidOutputs(fluid('isopropyl_alcohol') * 3000)
+        .fluidOutputs(fluid('isopropyl') * 1000)
         .duration(500)
         .EUt(256)
         .buildAndRegister()
 
 
-// KOH
+// Potassium Hydroxide
 recipemap('chemical_reactor').recipeBuilder()
         .fluidInputs(liquid('water') * 2000)
         .inputs(item('gregtech:meta_dust', 83) * 2)
         .fluidOutputs(fluid('hydrogen') * 1000)
-        .outputs(item('pexologicaljourney:potassium_hydroxide'))
+        .outputs(item('gregtech:meta_dust'), 32005)
         .duration(200)
         .EUt(128)
         .buildAndRegister()
+// Carbon Disulfide
 
-// CS2
 recipemap('mixer').recipeBuilder()
         .inputs(item('gregtech:meta_dust', 18), item('gregtech:meta_dust', 103) * 2)
         .notConsumable([metaitem('circuit.integrated').withNbt(["Configuration": 1])])
-        .outputs(item('pexologicaljourney:carbon_disulfide'))
+        .outputs(item('gregtech:meta_dust', 32003))
         .duration(100)
         .EUt(8)
         .buildAndRegister()
 
 
 // PEX
+
 recipemap('chemical_reactor').recipeBuilder()
-        .inputs(item('pexologicaljourney:carbon_disulfide') * 1, item('pexologicaljourney:potassium_hydroxide') * 1)
-        .fluidInputs(fluid('isopropyl_alcohol') * 1000)
+        .inputs(item('gregtech:meta_dust', 32003) * 1, item('gregtech:meta_dust', 32005) * 1)
+        .fluidInputs(fluid('isopropyl') * 1000)
         .outputs(item('gregtech:meta_dust', 32000))
         .fluidOutputs(fluid('water') * 1000)
         .duration(500)
