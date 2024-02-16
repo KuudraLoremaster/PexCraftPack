c22541a875198295de9a55f15175de886a63a8e6
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2024-02-16 08:30:15 +0100

    Delete main.groovy cuz it has 0 reason to exist

--- groovy/postInit/main.groovy
+++
@@ -1,6 +0,0 @@
-// crafting.addShaped('fuckandcock', item('minecraft:diamond') * 1[
-//         [metaItem('gregtech:cableGtSingleCopper'), metaItem('gregtech:plateRedstone'), metaItem('gregtech:cableGtSingleCopper')],
-//         [metaItem('gregtech:cableGtSingleCopper'), metaItem('gregtech:plateRedstone'), metaItem('gregtech:cableGtSingleCopper')],
-//         [metaItem('gregtech:cableGtSingleCopper'), metaItem('gregtech:plateRedstone'), metaItem('gregtech:cableGtSingleCopper')]
-
-// ])
