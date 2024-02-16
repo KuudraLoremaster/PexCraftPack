0093cba0a1802532785cd985463ee18a9d03577c
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2024-02-16 08:35:53 +0100

    reenable lex pex and rex skips and make them uv with better yield

--- groovy/postInit/recipesChemicalReactor.groovy
+++ groovy/postInit/recipesChemicalReactor.groovy
@@ -1,27 +1,27 @@
-// recipemap('large_chemical_reactor').recipeBuilder()
-//         .fluidInputs(liquid('hydrogen')*720,liquid('lithium')*144,liquid('oxygen')*144) //replace this with a real chain
-//         .inputs(item('gregtech:meta_dust', 18),item('gregtech:meta_dust', 103) * 2)
-//         .notConsumable([metaitem('circuit.integrated').withNbt(["Configuration": 1])])
-//         .outputs(item('pexologicaljourney:lex_ingot') * 12)
-//         .duration(500)
-//         .EUt(256)
-//         .buildAndRegister()
+recipemap('large_chemical_reactor').recipeBuilder()
+         .fluidInputs(liquid('hydrogen')*720,liquid('lithium')*144,liquid('oxygen')*144) //replace this with a real chain
+         .inputs(item('gregtech:meta_dust', 18),item('gregtech:meta_dust', 103) * 2)
+         .notConsumable([metaitem('circuit.integrated').withNbt(["Configuration": 1])])
+         .outputs(item('pexologicaljourney:lex_ingot') * 15)
+         .duration(500)
+         .EUt(524288)
+         .buildAndRegister()
 
-// recipemap('large_chemical_reactor').recipeBuilder()
-//         .fluidInputs(liquid('hydrogen')*720,liquid('potassium')*144,liquid('oxygen')*144) //replace this with a real chain
-//         .inputs(item('gregtech:meta_dust', 18),item('gregtech:meta_dust', 103) * 2, )
-//         .notConsumable([metaitem('circuit.integrated').withNbt(["Configuration": 1])])
-//         .outputs(item('pexologicaljourney:pex_ingot') * 12)
-//         .duration(500)
-//         .EUt(2048)
-//         .buildAndRegister()
+ recipemap('large_chemical_reactor').recipeBuilder()
+         .fluidInputs(liquid('hydrogen')*720,liquid('potassium')*144,liquid('oxygen')*144) //replace this with a real chain
+         .inputs(item('gregtech:meta_dust', 18),item('gregtech:meta_dust', 103) * 2, )
+         .notConsumable([metaitem('circuit.integrated').withNbt(["Configuration": 1])])
+         .outputs(item('pexologicaljourney:pex_ingot') * 15)
+         .duration(500)
+         .EUt(524288)
+         .buildAndRegister()
 
-// recipemap('large_chemical_reactor').recipeBuilder()
-//         .fluidInputs(liquid('hydrogen')*720,liquid('rhodium')*144,liquid('oxygen')*144) //replace this with a real chain
-//         .inputs(item('gregtech:meta_dust', 18),item('gregtech:meta_dust', 103) * 2)
-//         .notConsumable([metaitem('circuit.integrated').withNbt(["Configuration": 1])])
-//         .outputs(item('pexologicaljourney:rex_ingot') * 12)
-//         .duration(500)
-//         .EUt(32768)
-//         .buildAndRegister()
+ recipemap('large_chemical_reactor').recipeBuilder()
+         .fluidInputs(liquid('hydrogen')*720,liquid('rhodium')*144,liquid('oxygen')*144) //replace this with a real chain
+         .inputs(item('gregtech:meta_dust', 18),item('gregtech:meta_dust', 103) * 2)
+         .notConsumable([metaitem('circuit.integrated').withNbt(["Configuration": 1])])
+         .outputs(item('pexologicaljourney:rex_ingot') * 15)
+         .duration(500)
+         .EUt(524288)
+         .buildAndRegister()
  
