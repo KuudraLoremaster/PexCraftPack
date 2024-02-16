4d491a93a00da99c92f580aea42eca6e534a8abe
KuudraLoremaster <146659154+KuudraLoremaster@users.noreply.github.com>
2024-02-16 14:12:37 +0200

    pex is fucking real chat, pex isreal

--- groovy/preInit/metaItems.groovy
+++ groovy/preInit/metaItems.groovy
@@ -4,12 +4,12 @@ import net.minecraft.util.ResourceLocation
 import static gregtech.api.unification.material.info.MaterialFlags.*
 
 event_manager.listen { MaterialEvent event ->
-        def pex = new Material.Builder(32000, resource('pexologicaljourney', 'pex'))
+        def pex = new Material.Builder(32000, resource('pj', 'pex'))
                 .ingot()
+                .blastTemp(2000, 'LOW', 1024, 400)
                 .fluid()
-                .color(0xad9fd6)
                 .iconSet("shiny")
-                .flags('generate_plate', 'generate_gear', 'generate_rod')
+                .flags('generate_plate', 'generate_rod', 'generate_gear')
                 .build();
-                pex.setFormula("PEX10", true);
+        pex.setFormula("C3H8OCS2K", true);
 }
