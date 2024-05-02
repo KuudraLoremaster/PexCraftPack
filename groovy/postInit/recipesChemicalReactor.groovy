7f446e4f44a789cedfdc2763aa2896a1c2b59f7c
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2024-05-02 15:36:54 +0200

    real

--- groovy/postInit/recipesChemicalReactor.groovy
+++ groovy/postInit/recipesChemicalReactor.groovy
@@ -1,3 +1,4 @@
+/*
 recipemap('large_chemical_reactor').recipeBuilder()
          .fluidInputs(liquid('hydrogen')*720,liquid('lithium')*144,liquid('oxygen')*144) //replace this with a real chain
          .inputs(item('gregtech:meta_dust', 18),item('gregtech:meta_dust', 103) * 2)
@@ -24,4 +25,13 @@ recipemap('large_chemical_reactor').recipeBuilder()
          .duration(500)
          .EUt(524288)
          .buildAndRegister()
- 
+ */
+ recipemap('large_chemical_reactor').recipeBuilder()
+         .fluidInputs(liquid('ethanol')*720) //replace this with a real chain
+         .inputs(item('gregtech:carbondislufide', 18)* 2) //replace this shit
+         .notConsumable([metaitem('circuit.integrated').withNbt(["Configuration": 1])])
+         .outputs(item('pexologicaljourney:Sulfurized_Ethanol ') * 15)
+         .duration(500)
+         .EUt(524288)
+         .buildAndRegister()
+
