77f84f5f9d9110e65aed25defa3a317f00d4707c
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2024-10-14 10:17:06 +0200

    retardation fix uno

--- groovy/postInit/immersive_engineering/coke_oven.groovy
+++ groovy/postInit/immersive_engineering/coke_oven.groovy
@@ -1,10 +1,36 @@
-mods.immersiveengineering.coke_oven.add(item('gregtech:coke'), item('minecraft:coal'), 200, 30)
-mods.immersiveengineering.coke_oven.add(item('gregtech:coke'), item('minecraft:charcoal'), 200, 20)
-mods.immersiveengineering.coke_oven.add(item('minecraft:charcoal'), item('minecraft:log'), 100, 10)
-mods.immersiveengineering.coke_oven.add(item('gregtech:coke', 9), item('minecraft:coal_block'), 1600, 270)
+// Add Coke Oven recipes using recipeBuilder()
+mods.immersiveengineering.coke_oven.recipeBuilder()
+    .input(item('minecraft:coal'))
+    .output(item('gregtech:coke'))
+    .time(200)
+    .creosote(30)
+    .register()
 
+mods.immersiveengineering.coke_oven.recipeBuilder()
+    .input(item('minecraft:charcoal'))
+    .output(item('gregtech:coke'))
+    .time(200)
+    .creosote(20)
+    .register()
+
+mods.immersiveengineering.coke_oven.recipeBuilder()
+    .input(item('minecraft:log'))
+    .output(item('minecraft:charcoal'))
+    .time(100)
+    .creosote(10)
+    .register()
+
+mods.immersiveengineering.coke_oven.recipeBuilder()
+    .input(item('minecraft:coal_block'))
+    .output(item('gregtech:coke', 9))
+    .time(1600)
+    .creosote(270)
+    .register()
+
+// Remove Coke Oven recipes by input or output
 mods.immersiveengineering.coke_oven.removeByInput(item('minecraft:log'))
 mods.immersiveengineering.coke_oven.removeByOutput(item('immersiveengineering:coke'))
 
+// Add and remove Blast Furnace Fuel
 mods.immersiveengineering.blast_furnace_fuel.add(item('gregtech:coke'), 1600)
 mods.immersiveengineering.blast_furnace_fuel.removeByInput(item('minecraft:coal'))
