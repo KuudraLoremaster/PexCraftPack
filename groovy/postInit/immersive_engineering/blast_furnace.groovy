17d1a83e0a3de1b9e38d26bdc9372f1d12e101c8
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2024-10-14 10:19:36 +0200

    retardation fix dos

--- groovy/postInit/immersive_engineering/blast_furnace.groovy
+++ groovy/postInit/immersive_engineering/blast_furnace.groovy
@@ -1,16 +1,54 @@
-mods.immersiveengineering.blast_furnace.add(item('minecraft:iron_ingot'), oredict('oreIron'), 20, item('gregtech:ash'))
-mods.immersiveengineering.blast_furnace.add(item('minecraft:iron_ingot'), oredict('oreMagnetite'), 20, item('gregtech:ash'))
-mods.immersiveengineering.blast_furnace.add(item('gregtech:steel_ingot'), item('minecraft:iron_ingot'), 60, item('gregtech:ash'))
-mods.immersiveengineering.blast_furnace.add(item('gregtech:copper_ingot'), oredict('oreCopper'), 20, item('gregtech:ash'))
-mods.immersiveengineering.blast_furnace.add(item('gregtech:copper_ingot'), oredict('oreTetrahedrite'), 20, item('gregtech:ash'))
-mods.immersiveengineering.blast_furnace.add(item('gregtech:aluminum_nugget'), oredict('oreBauxite'), 10000, item('gregtech:ash'))
-mods.immersiveengineering.blast_furnace.add(item('gregtech:aluminum_nugget'), oredict('oreAluminium'), 20000, item('gregtech:ash'))
-mods.immersiveengineering.blast_furnace.add(item('gregtech:lead_ingot'), oredict('oreLead'), 20, item('gregtech:ash'))
+// Add recipes
+mods.immersiveengineering.blast_furnace.recipeBuilder()
+    .input(ore('oreMagnetite'))
+    .output(item('minecraft:iron_ingot'))
+    .time(20)
+    .slag(item('gregtech:meta_dust', 254))
+    .register()
 
-mods.immersiveengineering.blast_furnace.removeByInput(oredict('oreIron'))
-mods.immersiveengineering.blast_furnace.removeByInput(oredict('minecraft:iron'))
-mods.immersiveengineering.blast_furnace.removeByInput(oredict('minecraft:iron_block'))
+mods.immersiveengineering.blast_furnace.recipeBuilder()
+    .input(item('minecraft:iron_ingot'))
+    .output(item('gregtech:steel_ingot'))
+    .time(60)
+    .slag(item('gregtech:meta_dust', 254))
+    .register()
+
+mods.immersiveengineering.blast_furnace.recipeBuilder()
+    .input(ore('oreCopper'))
+    .output(item('gregtech:copper_ingot'))
+    .time(20)
+    .slag(item('gregtech:meta_dust', 254))
+    .register()
+
+mods.immersiveengineering.blast_furnace.recipeBuilder()
+    .input(ore('oreTetrahedrite'))
+    .output(item('gregtech:copper_ingot'))
+    .time(20)
+    .slag(item('gregtech:meta_dust', 254))
+    .register()
+
+mods.immersiveengineering.blast_furnace.recipeBuilder()
+    .input(ore('oreBauxite'))
+    .output(item('gregtech:aluminum_nugget'))
+    .time(10000)
+    .slag(item('gregtech:meta_dust', 254))
+    .register()
+
+mods.immersiveengineering.blast_furnace.recipeBuilder()
+    .input(ore('oreAluminium'))
+    .output(item('gregtech:aluminum_nugget'))
+    .time(20000)
+    .slag(item('gregtech:meta_dust', 254))
+    .register()
+
+mods.immersiveengineering.blast_furnace.recipeBuilder()
+    .input(ore('oreLead'))
+    .output(item('gregtech:lead_ingot'))
+    .time(20)
+    .slag(item('gregtech:meta_dust', 254))
+    .register()
+
+// Remove recipes by input or output
+mods.immersiveengineering.blast_furnace.removeByInput(ore('minecraft:iron'))
+mods.immersiveengineering.blast_furnace.removeByInput(ore('minecraft:iron_block'))
 mods.immersiveengineering.blast_furnace.removeByOutput(item('immersiveengineering:slag'))
-
-
-
