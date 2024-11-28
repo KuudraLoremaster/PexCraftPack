53727862830ac493f70d3c9cf530deab343c78ee
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2024-11-28 13:10:32 +0100

    Update Sexer.groovy

--- groovy/postInit/CustomMultiblocks/Sexer.groovy
+++ groovy/postInit/CustomMultiblocks/Sexer.groovy
@@ -1,1 +1,52 @@
+import gregtech.api.metatileentity.MetaTileEntity
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity
+import gregtech.api.metatileentity.multiblock.IMultiblockPart
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController
+import gregtech.api.pattern.BlockPattern
+import gregtech.api.pattern.FactoryBlockPattern
+import gregtech.api.recipes.RecipeMaps
+import gregtech.client.renderer.ICubeRenderer
+import gregtech.client.renderer.texture.Textures
+import gregtech.common.blocks.BlockMetalCasing.MetalCasingType
+import gregtech.common.blocks.MetaBlocks
 
+class MetaTileEntitySexer extends RecipeMapMultiblockController {
+
+    /*(1)!*/
+    MetaTileEntitySexer(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, RecipeMaps.SEXER_RECIPES) 
+    }
+
+    @Override
+    MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntitySexer(metaTileEntityId) /*(3)!*/
+    }
+
+    @Override
+    protected BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start() /*(4)!*/
+                .aisle("XXX", "XXX", "XXX") //
+                .aisle("XXX", "X#X", "XXX") //
+                .aisle("XXX", "XSX", "XXX") //
+
+                .where('S', selfPredicate())
+                .where('X', states(getCasingState()).setMinGlobalLimited(14).or(autoAbilities()))
+                .where('#', air())
+                .build()
+    }
+
+    @Override
+    ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return Textures.BRICKED_BRONZE_CASING /*(5)!*/
+
+    /*(6)!*/
+    protected IBlockState getCasingState() {
+        return MetaBlocks.METAL_CASING.getState(MetalCasingType.BRONZE_BRICKED)
+    }
+
+    /*(7)!*/
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.SEXER_OVERLAY
+    }
+}
