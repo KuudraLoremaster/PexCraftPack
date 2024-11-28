16943a74d142f7742d179dee329dc3dd35446c62
uwudwagon <146089002+zeldaLorddActivateWindows@users.noreply.github.com>
2024-11-28 12:59:04 +0100

    Update Mexer.groovy

--- groovy/postInit/CustomMultiblocks/Mexer.groovy
+++ groovy/postInit/CustomMultiblocks/Mexer.groovy
@@ -1,1 +1,54 @@
+import gregtech.api.metatileentity.MetaTileEntity
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity
+import gregtech.api.metatileentity.multiblock.IMultiblockPart
+import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController
+import gregtech.api.pattern.BlockPattern
+import gregtech.api.pattern.FactoryBlockPattern
+import gregtech.api.recipes.RecipeMaps
+import gregtech.client.renderer.ICubeRenderer
+import gregtech.client.renderer.texture.Textures
+import gregtech.common.blocks.BlockMetalCasing.MetalCasingType
+import gregtech.common.blocks.MetaBlocks
 
+class MetaTileEntityLexer extends RecipeMapMultiblockController {
+
+    /*(1)!*/
+    MetaTileEntityLexer(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, RecipeMaps.MEXER_RECIPES) 
+    }
+
+    @Override
+    MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
+        return new MetaTileEntityMexer(metaTileEntityId) /*(3)!*/
+    }
+
+    @Override
+    protected BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start() /*(4)!*/
+                .aisle("XXX", "XXX", "XXXXX") //
+                .aisle("XXX", "X#X", "XXGGX") //
+                .aisle("XXX", "XSX", "XXXXX") //
+
+                .where('S', selfPredicate())
+                .where('X', states(getCasingState()).setMinGlobalLimited(14).or(autoAbilities()))
+                .where('#', air())
+                .where('G', titaniumGearBox())
+                .build()
+    }
+
+    @Override
+    ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return Textures.STABLE_TITANIUM_CASING /*(5)!*/
+    }
+
+    /*(6)!*/
+    protected IBlockState getCasingState() {
+        return MetaBlocks.TITANIUM_CASING.getState(MetalCasingType.TITANIUM_SOLID)
+    }
+
+    /*(7)!*/
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.LEXER_OVERLAY
+    }
+}
